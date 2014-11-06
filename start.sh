334f2fb4c66edcf9e28859df20fe35256e142807
Chaniel <Chaniel@users.noreply.github.com>
2014-11-06 11:16:12 +0800

    Create start.sh
    start shell script

---
+++ start.sh
@@ -0,0 +1,1 @@
+/data/mysql-5.6.17/bin/mysqld --defaults-file=my.cnf --basedir=/data/mysql-5.6.17 --datadir=/data/mysql-5.6.17/data --plugin-dir=/data/mysql-5.6.17/lib/plugin --user=mysql --log-error=/data/mysql-5.6.17/data/SHUBEI-1-100.err --pid-file=/data/mysql-5.6.17/data/SHUBEI-1-100.pid &
