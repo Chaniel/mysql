5866c02aad70bd264aedbe8fb45d2d5c2272c663
Chaniel <Chaniel@users.noreply.github.com>
2014-11-10 10:20:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,8 @@
 
 base_dir=/data/mysql-1
 $base_dir/bin/mysqld --defaults-file=$base_dir/my.cnf --basedir=$base_dir --datadir=$base_dir/data --plugin-dir=$base_dir/lib/plugin --user=mysql --log-error=$base_dir/data/SHUBEI-1-100.err --pid-file=$base_dir/data/SHUBEI-1-100.pid --socket=$base_dir/data/mysql.sock &
+
+
+
+# --skip-grant-tables 
+# for upgrade mysql replication.
