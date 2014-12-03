420feb77979ba51aecc0f6a35d9cb20663938f4f
Chaniel <Chaniel@users.noreply.github.com>
2014-12-03 17:12:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ base_dir=/data/mysql-1
 $base_dir/bin/mysqld --defaults-file=$base_dir/my.cnf --basedir=$base_dir --datadir=$base_dir/data --plugin-dir=$base_dir/lib/plugin --user=mysql --log-error=$base_dir/data/SHUBEI-1-100.err --pid-file=$base_dir/data/SHUBEI-1-100.pid --socket=$base_dir/data/mysql.sock &
 
 
+#/bin/sh bin/mysqld_safe --defaults-file=my.cnf
 
+#Restart them with the --skip-slave-start option so that they do not connect to the master. Perform any table repair or rebuilding operations needed to re-create database objects, 
+#use the --skip-networking option when you restart the master.
 # --skip-grant-tables 
 # for upgrade mysql replication.
